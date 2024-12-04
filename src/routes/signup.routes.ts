3d32df3ba25b7a9dcddc9bad80a5a2aa8fad7cfc
Kieran Mai <kieran.noel@icloud.com>
2024-12-04 10:29:44 +0100

    create testcase

--- src/routes/signup.routes.ts
+++ src/routes/signup.routes.ts
@@ -1,25 +1,12 @@
 import { Router } from 'express';
-import environment from '../environments/environment';
-import { Users } from '../models';
 
 const router = Router();
-const JWT_SECRET = environment.JWT_SECRET;
 
-router.post('/signup', async (req, res) => {
-  const userData = new Users(req.body);
+router.post('/testCookie', async (req, res) => {
+  console.log(req.cookies);
+  res.clearCookie('refreshToken');
 
-  //   if (!userData) {
-  //     res.sendStatus(400);
-  //   }
-  //   try {
-  //     const newUser = await users.save();
-  //     res.send({ status: 'OK', profilPicSrc: newCustomer.profilPicSrc, _id: newCustomer._id });
-  //     return;
-  //   } catch (error) {
-  //     console.log(error);
-  //     res.send({ status: 'Error' });
-  //     return;
-  //   }
+  res.sendStatus(200);
 });
 
 export default router;
